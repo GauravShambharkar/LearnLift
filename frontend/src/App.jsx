5eb043f7ce9d7cd7ba586c77798b111fbeeb969a
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-08-02 19:24:12 +0530

    ui libraries init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Nav from "./Nav";
 import Registerform from "./components/Registerform";
 
 
@@ -10,8 +9,8 @@ function App() {
   return (
     <>
       <div className="w-full h-screen bg-[white]">
-        <Nav/>
-        
+        {/* <Nav/> */}
+        <Registerform/>
       </div>
     </>
   );
