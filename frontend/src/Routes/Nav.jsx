6ba6cb41706c78b4d95229e5bc5aae8d3b00d6cd
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-08-02 20:37:53 +0530

    isActive?

--- frontend/src/Routes/Nav.jsx
+++ frontend/src/Routes/Nav.jsx
@@ -5,11 +5,39 @@ import Routing from "./Routing";
 const Nav = () => {
   return (
     <>
-      <div className="flex justify-end items-center p-4 border gap-4 font-medium  bg-gray-800 text-white">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/profile">Profile</NavLink>
-        <NavLink to="/course">Course</NavLink>
-        <NavLink to="/register">Register</NavLink>
+      <div className="flex justify-end items-center p-4 gap-4 font-medium bg-black text-white">
+        <NavLink
+          to="/"
+          className={({ isActive }) => {
+            return isActive ? "text-blue-500" : "text-white";
+          }}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/profile"
+          className={({ isActive }) => {
+            return isActive ? "text-blue-500" : "text-white";
+          }}
+        >
+          Profile
+        </NavLink>
+        <NavLink
+          to="/course"
+          className={({ isActive }) => {
+            return isActive ? "text-blue-500" : "text-white";
+          }}
+        >
+          Course
+        </NavLink>
+        <NavLink
+          to="/register"
+          className={({ isActive }) => {
+            return isActive ? "text-blue-500" : "text-white";
+          }}
+        >
+          Register
+        </NavLink>
       </div>
       <Routing />
     </>
